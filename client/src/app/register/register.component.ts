587931c9ebb19dd4c2092910c27f3dcc249f22c2
Irakli <iraklichabiev7@gmail.com>
2022-12-26 19:22:31 +0400

    Added Home and Registration (sect5)

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit{
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService){}
+
+  ngOnInit(){
+  }
+
+  register(){
+    this.accountService.register(this.model).subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => this.cancel()
+    })
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+}
